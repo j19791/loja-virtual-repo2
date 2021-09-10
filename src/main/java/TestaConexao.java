8a91ec744d92cebf3081695fea6a96670775cb27
Jefferson <jeffrm@gmail.com>
2021-09-10 20:56:18 -0300

    Aula 1 - Configurando ambiente

---
+++ src/main/java/TestaConexao.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+
+
+		Connection connection = DriverManager
+				.getConnection("jdbc:mysql://database-1.cmblgshfej2r.us-east-2.rds.amazonaws.com:3306/loja_virtual", "admin", "H8NJJaUJkKm9Qv3");
+
+		
+		
+		
+		connection.close();
+		
+		System.out.println("teste");
+		
+	}
+
+}
